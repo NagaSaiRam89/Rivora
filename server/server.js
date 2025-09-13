5e25313705b88cc40b435445b522cd9dea01d84f
Naga Sai Ram <ramhere939@gmail.com>
2025-09-13 11:14:33 +0530

    Added cors fix

--- server/server.js
+++ server/server.js
@@ -11,10 +11,7 @@ const mongoose = require('mongoose');
 dotenv.config();
 
 const app = express();
-app.use(cors({
-  origin: [process.env.FRONTEND_URL, 'http://localhost:5173'], 
-  credentials: true 
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use(cookieParser());
