b793ccff7db3e5f23eca5253985a7e967aaf62b4
Naga Sai Ram <ramhere939@gmail.com>
2025-09-13 11:57:15 +0530

    bug fixes cookie

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -40,7 +40,7 @@ const loginUser = async (req, res) => {
       httpOnly: true,
       secure: process.env.NODE_ENV === 'production',
       sameSite: 'None',
-      maxAge: 5,
+      maxAge: 24 * 60 * 60 * 1000,
     })
 
     res.status(200).json({ message: 'Login successful', user: user })
