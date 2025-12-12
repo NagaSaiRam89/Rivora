fbbdd0285a5d2605f7b1e96a7272789059bbd68a
SUNKARA NAGA SAI RAM <snsram089.mca2023@ca.nitrr.ac.in>
2025-12-12 18:12:47 +0530

    Enable secure connection for email transports
    Stmp_port_block_Fix

--- server/controllers/sessionController.js
+++ server/controllers/sessionController.js
@@ -11,6 +11,7 @@ const { videoQueue } = require('../queues/videoQueue')
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: parseInt(process.env.SMTP_PORT || '587'),
+  secure: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
